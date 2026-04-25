06d6e54f6f7da43cffa950a784d49e29eb38b988
Steve Kaser <stevekaser@protonmail.com>
2026-04-25 14:09:46 -0400

    removed raylib 6 because it wouldn't compile; using 5.5

--- raylib/main.c
+++ raylib/main.c
@@ -6,12 +6,18 @@
 #define NOB_IMPLEMENTATION
 #include "./nob.h"
 
-#define SPEED_MAX 2 
+#define ASSETS_DIR "../assets"
+#define QR_DIR ASSETS_DIR"/images/number-qrcodes"
+#define DM_DIR ASSETS_DIR"/images/alphabet-data-matrix"
+
+#define BOX_OUTLINE_THICCNESS 1
+#define SPEED_MAX 5 
+#define BARCODE_SIZE 75
 
 typedef struct {
-  Rectangle rect;
+  Vector2 pos;
   Vector2 velo;
-  const char* name; // temporary until we get images going
+  Texture2D tex;
 } Barcode;
 
 typedef struct {
@@ -20,61 +26,33 @@ typedef struct {
   size_t count;
 } Barcodes;
 
+typedef struct {
+  char prefix;
+  String_View *sv;
+} Scan;
+
+Rectangle get_barcode_rect(Barcode b) {
+  return (Rectangle) { .x = b.pos.x, .y = b.pos.y, .width = b.tex.width, .height = b.tex.height };
+}
+
 void draw_barcode(Barcode b) {
-  DrawRectangleLinesEx(b.rect, 1, LIME);
+  DrawTextureEx(b.tex, b.pos, 0, 1, WHITE);
+  DrawRectangleLinesEx(get_barcode_rect(b), BOX_OUTLINE_THICCNESS, LIME);
 }
 
 void update_barcode(Barcode *me) {
-  if (me->rect.x + me->velo.x < 0 || me->rect.x + me->rect.width + me->velo.x > GetScreenWidth()) {
+  if (me->pos.x + me->velo.x < 0 || me->pos.x + me->tex.width + BOX_OUTLINE_THICCNESS + me->velo.x > GetScreenWidth()) {
     me->velo.x *= -1; 
   }
- 
-  
-  if (me->rect.y + me->velo.y < 0 || me->rect.y + me->rect.height + me->velo.y> GetScreenHeight()) {
+
+
+  if (me->pos.y + me->velo.y < 0 || me->pos.y + me->tex.height + BOX_OUTLINE_THICCNESS + me->velo.y> GetScreenHeight()) {
     me->velo.y *= -1; 
   }
 
-  me->rect.x += me->velo.x;
-  me->rect.y += me->velo.y;
-  
-}
+  me->pos.x += me->velo.x;
+  me->pos.y += me->velo.y;
 
-void check_barcode_collisions(Barcode *me, Barcodes *bars) {
-  da_foreach(Barcode, b, bars) {
-    if (me != b) {
-      if (CheckCollisionRecs(me->rect, b->rect)) {
-        Rectangle cRec = GetCollisionRec(me->rect, b->rect);
-        //DrawRectangleRec(cRec, RED);
-        if (cRec.width >= cRec.height) {
-          me->velo.x *= -1;
-          b->velo.x *= -1;
-          if (me->velo.x < 0) {
-            me->rect.x -= cRec.width;
-          } else {
-            me->rect.x += cRec.width;
-          }
-          if (b->velo.x < 0) {
-            b->rect.x -= cRec.width;
-          } else {
-            b->rect.x += cRec.width;
-          }
-        } else {
-          me->velo.y *= -1;
-          b->velo.y *= -1;
-          if (me->velo.y < 0) {
-            me->rect.y -= cRec.height;
-          } else {
-            me->rect.y += cRec.height;
-          }
-          if (b->velo.y < 0) {
-            b->rect.y -= cRec.height;
-          } else {
-            b->rect.y += cRec.height;
-          }
-        }
-      }
-    }
-  }
 }
 
 int get_random_non_zero_int(int _min, int _max) {
@@ -83,34 +61,112 @@ int get_random_non_zero_int(int _min, int _max) {
   return v;
 }
 
+void load_barcodes(const char* fp, Barcodes *bars) {
+  FilePathList files = LoadDirectoryFiles(fp);
+  for (unsigned int i = 0; i < files.count; ++i) {
+    const char *f = files.paths[i];
+    Image img = LoadImage(f);
+    ImageResize(&img, BARCODE_SIZE, BARCODE_SIZE);
+    Texture2D tex = LoadTextureFromImage(img);
+    UnloadImage(img);
+
+    Barcode b = (Barcode) { 
+      (Vector2) { get_random_non_zero_int(100, GetScreenWidth() - 500), 
+        get_random_non_zero_int(100, GetScreenHeight() - 500) },
+        (Vector2) { get_random_non_zero_int(-SPEED_MAX, SPEED_MAX), 
+          get_random_non_zero_int(-SPEED_MAX, SPEED_MAX) },
+        tex 
+    };
+    da_append(bars, b);
+  }
+}
+
+String_View *alloc_string_view() {
+  String_View *sv = (String_View*)malloc(sizeof(String_View));
+  memset(sv, 0, sizeof(*sv));
+
+  return sv;
+}
+
+Scan *alloc_scan() {
+  Scan *s = (Scan*)malloc(sizeof(Scan));
+  memset(s, 0, sizeof(*s));
+
+  s->sv = alloc_string_view();
+
+  return s;
+}
+
+Scan *process_scan(String_Builder sb) {
+  if (sb.count == 0) return NULL;
+  if ((char)sb.items[0] != (char)KEY_LEFT_SHIFT) {
+    nob_log(ERROR, "Invalid prefix. Expected %c. Got %c", KEY_LEFT_SHIFT, sb.items[0]);
+    return NULL;
+  }
+
+  char prefix = '0';
+  switch (sb.items[1]) {
+    case 48: prefix = ')'; break;
+    case 49: prefix = '!'; break;
+    case 50: prefix = '@'; break;
+    case 51: prefix = '#'; break;
+    case 52: prefix = '$'; break;
+    case 53: prefix = '%'; break;
+    case 54: prefix = '^'; break;
+    case 55: prefix = '&'; break;
+    case 56: prefix = '*'; break;
+    case 57: prefix = '('; break;
+    default: {
+      nob_log(ERROR, "Invalid prefix character. Expected one of 0-9. Got %c", sb.items[1]);
+      return NULL;
+    }
+  }
+
+  String_View *sv = alloc_string_view();
+  sv->count = sb.count;
+  sv->data = sb.items; 
+  *sv = sv_chop_right(sv, sb.count-2);
+
+  Scan *s = alloc_scan();
+  s->prefix = prefix;
+  s->sv = sv;
+  return s;
+
+}
+
 int main() {
-  
-  InitWindow(800, 600, "Scanner Wars");
+
+  InitWindow(1920, 1080, "Scanner Wars");
   SetTargetFPS(60);
+
   struct timespec ts;
   clock_gettime(CLOCK_MONOTONIC, &ts);
   SetRandomSeed(ts.tv_nsec);
-  
+
   Barcodes bars = {0};
-  size_t temp_bc_size = 50;
-  for (size_t i = 0; i < 5; ++i) {
-    Barcode b = (Barcode) { (Rectangle) { i*temp_bc_size+20*i, 100,
-                                          temp_bc_size, temp_bc_size},
-                            (Vector2) { get_random_non_zero_int(-SPEED_MAX, SPEED_MAX), 
-                                        get_random_non_zero_int(-SPEED_MAX, SPEED_MAX) },
-                            temp_sprintf("%ld", i)};
-    da_append(&bars, b);
-    nob_log(INFO, "Velo: %f, %f", b.velo.x, b.velo.y);
-  }
-  
+  load_barcodes(QR_DIR, &bars);
+  load_barcodes(DM_DIR, &bars);
+
+  String_Builder buff = {0};
+
   while (!WindowShouldClose()) {
 
-    da_foreach(Barcode, b, &bars) {
-      update_barcode(b);
+    int k = GetKeyPressed();
+    while (k > 0) {
+      if (k == KEY_ENTER) {
+        Scan *scan = process_scan(buff);
+        if (scan) {
+          nob_log(INFO, "\nPREFIX: %c\nSCAN: "SV_Fmt, scan->prefix, SV_Arg(*scan->sv));
+        }
+        buff.count = 0;
+      } else {
+        sb_append(&buff, (char)k);
+      }
+      k = GetKeyPressed();
     }
 
     da_foreach(Barcode, b, &bars) {
-      check_barcode_collisions(b, &bars);
+      update_barcode(b);
     }
 
     BeginDrawing();
@@ -119,9 +175,13 @@ int main() {
     da_foreach(Barcode, b, &bars) {
       draw_barcode(*b);
     }
-    
+
     EndDrawing();
 
+  }
+
+  da_foreach(Barcode, b, &bars) {
+    UnloadTexture(b->tex);
   }
 
   CloseWindow();
--- raylib/nob.c
+++ raylib/nob.c
@@ -1,31 +1,24 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
 
+#define RAYLIB_VERSION "5.5"
+
 int main(int argc, char** argv) {
   
   GO_REBUILD_URSELF(argc, argv);
 
   Cmd cmd = {0};
-
+  
   cmd_append(&cmd, "cc");
   cmd_append(&cmd, "-Wall");
   cmd_append(&cmd, "-Wextra");
-  cmd_append(&cmd, "-o");
-  cmd_append(&cmd, "main");
-  cmd_append(&cmd, "main.c");
-  
-  //cmd_append(&cmd, "-lraylib", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lX11");
-
-  cmd_append(&cmd, "-I./raylib-6.0_linux_amd64/include/");
-  cmd_append(&cmd, "-L./raylib-6.0_linux_amd64/lib/");
+  cmd_append(&cmd, "-ggdb");
+  cmd_append(&cmd, "-I./raylib-"RAYLIB_VERSION"_linux_amd64/include/");
+  cmd_append(&cmd, "-o", "main", "main.c");
+  cmd_append(&cmd, "-L./raylib-"RAYLIB_VERSION"_linux_amd64/lib/");
   cmd_append(&cmd, "-l:libraylib.a");
-  cmd_append(&cmd, "-lpthread");
-  cmd_append(&cmd, "-ldl");
-  cmd_append(&cmd, "-lrt");
-  cmd_append(&cmd, "-lGL");
-  cmd_append(&cmd, "-lX11");
   cmd_append(&cmd, "-lm");
-
+  
   if (!cmd_run(&cmd)) return 1;
 
   return 0;
